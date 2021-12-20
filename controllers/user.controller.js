2a576ceadce322543464ed1d865c86fb64618e09
dhouhab <dhouha.bessalah@outlook.com>
2021-12-20 23:41:16 +0100

    password encryption+verification if user exists or not

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,36 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.userRegister = async (req, res) => {
-    const newUser = new User({ ...req.body });
 
+
+
+  const newUser = new User({ ...req.body });
+  const email = newUser.email;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(401).json({ msg: "user already exist" });
+  }
+
+ 
+
+/*
+
+console.log(req.body);
+const user = req.body;
+const password = user.password;
+
+*/
 
 try {
+  const salt = await bcrypt.genSalt(10);
+ 
+  const hash = await bcrypt.hash(newUser.password, salt);
+
+
+  newUser.password = hash;
+
 
 
     await newUser.save();
